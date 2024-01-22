e3ba086249b494f4efa21c86e853171b7bb20b2c
yoshiyasuyu <ry.studio.yoshi@gmail.com>
2024-01-23 01:33:02 +0900

    updated hw10

--- 2023_fall/lec10/homework10.py
+++ 2023_fall/lec10/homework10.py
@@ -16,7 +16,13 @@ def waveform_to_frames(waveform, frame_length, step):
     For every n and t such that 0 <= t*step+n <= N-1, it should be the case that 
        frames[n,t] = waveform[t*step+n]
     '''
-    raise RuntimeError("You need to change this part")
+    
+    num_frames = int((len(waveform)-frame_length)/step)
+    speech_frames = np.zeros((frame_length, num_frames))
+    for frame in range(num_frames):
+        speech_frames[:, frame] = waveform[frame*step:frame*step+frame_length]
+    return speech_frames
+
 
 def frames_to_stft(frames):
     '''
@@ -28,7 +34,9 @@ def frames_to_stft(frames):
     @returns:
     stft (np.ndarray((frame_length,num_frames))) - the STFT (complex-valued)
     '''
-    raise RuntimeError("You need to change this part")
+    #raise RuntimeError("You need to change this part")
+    stft = np.fft.fft(frames, axis=0)
+    return stft
 
 def stft_to_spectrogram(stft):
     '''
@@ -46,6 +54,8 @@ def stft_to_spectrogram(stft):
     np.amax(spectrogram) should be 0dB.
     np.amin(spectrogram) should be no smaller than -60dB.
     '''
-    raise RuntimeError("You need to change this part")
-
+    #raise RuntimeError("You need to change this part")
+    mstft = np.abs(stft)
+    speech_spectrogram = 20 * np.log10(np.maximum(0.001,mstft/np.amax(mstft)))
+    return speech_spectrogram
 
